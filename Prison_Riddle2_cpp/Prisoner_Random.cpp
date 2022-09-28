13f4bbc7c7ca166dd999445cd1cc52a8749cf355
Jason Raubenheimer <raubenheimer.jason@gmail.com>
2022-09-28 11:28:46 +0100

    Random and Loop sims are working

--- Prison_Riddle2_cpp/Prisoner_Random.cpp
+++ Prison_Riddle2_cpp/Prisoner_Random.cpp
@@ -2,13 +2,37 @@
 
 // Constructor
 Prisoner_Random::Prisoner_Random(const size_t number, const size_t num_prisoners, const size_t max_search_boxes)
-	: Prisoner{ number, num_prisoners, max_search_boxes }, searched_boxes{}
+	: Prisoner{ number, num_prisoners, max_search_boxes }, random_boxes(num_prisoners, 0)
 {
+	// initialise random_boxes vector
+
+	// set each value of the vector, starting at 0, and incrementing by 1
+	std::iota(random_boxes.begin(), random_boxes.end(), 0);
+
+	// shufle box numbers in boxes vector
+	// https://www.youtube.com/watch?v=oW6iuFbwPDg&ab_channel=TopShelfTechnology
+
+	std::ranges::shuffle(random_boxes, rd);
+
+	//std::cout << "random boxes size -- " << random_boxes.size() << std::endl;
 }
 
 bool Prisoner_Random::search_box(const size_t num_in_box)
 {
 	boxes_searched++;
-	box_to_search = num_in_box; // for next time
+	box_to_search = next_box_to_search(); // for next time
 	return is_number_found(num_in_box);
+}
+
+/*
+Return a random number (that hasnt already been selected)
+between 0 and num_boxes-1
+*/
+size_t Prisoner_Random::next_box_to_search()
+{
+
+	size_t temp{ random_boxes.back() };
+	//std::cout << "size of random_boxes: " << random_boxes.size() << " -- temp: " << temp << std::endl;
+	random_boxes.pop_back();
+	return temp;
 }
--- Prison_Riddle2_cpp/Prisoner_Random.h
+++ Prison_Riddle2_cpp/Prisoner_Random.h
@@ -1,12 +1,20 @@
 #pragma once
 
 #include <vector>
+#include <random>
+#include <numeric> // iota
+#include <ranges>
+#include <algorithm>
+#include <iostream>
+
 #include "Prisoner.h"
 
 class Prisoner_Random :public Prisoner
 {
-protected:
-	std::vector<size_t> searched_boxes;
+private:
+	std::vector<size_t> random_boxes;
+	size_t next_box_to_search();
+	std::random_device rd;
 
 public:
 	Prisoner_Random(const size_t number, const size_t num_prisoners, const size_t max_search_boxes);
